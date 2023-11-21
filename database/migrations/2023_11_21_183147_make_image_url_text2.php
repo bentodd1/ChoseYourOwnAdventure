c77aa0d566eb62f8ea65fa05f7efaeb4ce3a7832
Ben Todd <happynowbtodd@gmail.com>
2023-11-21 10:18:16 -0600

    image url migration

--- database/migrations/2023_11_21_183147_make_image_url_text2.php
+++ database/migrations/2023_11_21_183147_make_image_url_text2.php
@@ -11,7 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::table('adventure_pieces', function (Blueprint $table) {
+            $table->text("image_url")->change();
+        });
     }
 
     /**
