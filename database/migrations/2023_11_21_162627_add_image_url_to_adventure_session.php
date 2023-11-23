88e2b30e48368493556d26f3558c9053e5d2011b
Ben Todd <happynowbtodd@gmail.com>
2023-11-23 13:13:09 -0600

    Change to make the city of love.

--- database/migrations/2023_11_21_162627_add_image_url_to_adventure_session.php
+++ database/migrations/2023_11_21_162627_add_image_url_to_adventure_session.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('adventure_pieces', function (Blueprint $table) {
-            $table->text("image_url")->change();
+            $table->text("image_url");
         });
     }
 
